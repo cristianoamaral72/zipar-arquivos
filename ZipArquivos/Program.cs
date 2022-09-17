a07448ff8f847285d1b983cc7559e5d8b36df485
Cristiano Da Silva Ribeiro - Cadmus <cristiano.cadmus@tegma.com.br>
2022-09-17 14:55:24 -0300

    primeira versão

---
+++ ZipArquivos/Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.IO;
+using System.IO.Compression;
+
+namespace ZipArquivos
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string path = "C:\\";
+            //ZiparPastaArquivos(path);
+            DiziparPastaArquivos(path);
+        }
+
+        private static void ZiparPastaArquivos(string path)
+        {
+            string sourceDirctoty = $"{path}Documentos\\Arquivos MP3";
+
+            if (Directory.Exists(sourceDirctoty))
+            {
+                string archiveName = $"{path}Documentos\\DocumetosArchive.zip";
+                if (!File.Exists(archiveName))
+                {
+                    ZipFile.CreateFromDirectory(sourceDirctoty, archiveName, CompressionLevel.Optimal, false);
+                }
+            }
+        }
+
+        private static void DiziparPastaArquivos(string path)
+        {
+            string destinationDirectory = $"{path}Documentos\\DocumentsUnzipped";
+
+            if (Directory.Exists(path))
+            {
+                string archiveName = $"{path}Documentos\\DocumetosArchive.zip";
+                if (File.Exists(archiveName))
+                {
+                    ZipFile.ExtractToDirectory(archiveName, destinationDirectory);
+                }
+            }
+        }
+    }
+}
